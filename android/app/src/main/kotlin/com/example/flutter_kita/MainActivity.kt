06c907a8a6dce19d77f5fc632129ee6d7fb18ac1
WahabSyahranie <wahabschools@gmail.com>
2025-10-20 15:31:32 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_kita/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_kita
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kita/welcome_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "PT Kita",
+      home: WelcomeScreen(),
+    );
+  }
+}
