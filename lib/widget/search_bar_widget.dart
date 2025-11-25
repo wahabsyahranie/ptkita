af644c6b0d297b4afdb430df7df420ace93b1f07
Fahlevy <fahlevy289@gmail.com>
2025-11-25 23:18:12 +0800

    komentar search bar

--- lib/widget/search_bar_widget.dart
+++ lib/widget/search_bar_widget.dart
@@ -2,22 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_kita/styles/colors.dart';
 
 class SearchBarWidget extends StatelessWidget {
-  const SearchBarWidget({super.key});
+  const SearchBarWidget({
+    super.key,
+    required this.controller,
+    required this.onChanged,
+    this.hintText = 'Cari sesuatu...',
+  });
+
+  final TextEditingController controller;
+  final VoidCallback onChanged;
+  final String hintText;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 7),
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 7),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(24),
         border: Border.all(color: MyColors.secondary, width: 1.5),
+        color: Colors.white,
       ),
       child: Row(
         children: [
           Icon(Icons.search, color: MyColors.secondary),
           const SizedBox(width: 10),
+
+          // TEXTFIELD
           Expanded(
             child: TextField(
+              controller: controller,
+              onChanged: (_) => onChanged(),
               style: TextStyle(
                 color: MyColors.secondary,
                 fontWeight: FontWeight.w400,
@@ -25,7 +39,7 @@ class SearchBarWidget extends StatelessWidget {
               ),
               cursorColor: MyColors.secondary,
               decoration: InputDecoration(
-                hintText: 'Cari sesuatu...',
+                hintText: hintText,
                 hintStyle: TextStyle(
                   color: MyColors.secondary.withOpacity(0.7),
                   fontSize: 16,
@@ -34,6 +48,19 @@ class SearchBarWidget extends StatelessWidget {
               ),
             ),
           ),
+
+          // CLEAR BUTTON
+          if (controller.text.isNotEmpty)
+            GestureDetector(
+              onTap: () {
+                controller.clear();
+                onChanged();
+              },
+              child: Icon(
+                Icons.close_rounded,
+                color: MyColors.secondary.withOpacity(0.7),
+              ),
+            ),
         ],
       ),
     );
