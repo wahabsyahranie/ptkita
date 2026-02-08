9985f0421b13fede150bc5d0fb5dd1dbe869367d
Fahlevy <fahlevy289@gmail.com>
2026-02-08 18:19:46 +0800

    Logic &card history warranty, transaction new logic for warranty

--- lib/widget/navigation_drawer_widget.dart
+++ lib/widget/navigation_drawer_widget.dart
@@ -106,7 +106,7 @@ class NavigationDrawerWidget extends StatelessWidget {
       case 3:
         Navigator.of(
           context,
-        ).push(MaterialPageRoute(builder: (context) => DataGaransiPage()));
+        ).push(MaterialPageRoute(builder: (context) => WarrantyHistoryPage()));
         break;
     }
   }
