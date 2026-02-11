1fe59c9c694f67088a948eb23e91d9e4a0eef908
WahabSyahranie <wahabschools@gmail.com>
2026-02-12 05:12:25 +0800

    login page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_kita/core/app_router.dart';
+import 'package:flutter_kita/styles/colors.dart';
 import 'firebase_options.dart';
-import 'package:flutter_kita/pages/welcome_screen_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,8 +18,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "PT Kita",
-      theme: ThemeData(fontFamily: 'Poppins'),
-      home: WelcomeScreenPage(),
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: MyColors.secondary),
+      ),
+      home: const AppRouter(),
     );
   }
 }
